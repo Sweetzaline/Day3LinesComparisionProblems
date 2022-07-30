2a2ec47f44acc7aaa1f829128cdd3f74cdaec14a
unknown <buragohain4@gmail.com>
2022-07-31 03:18:02 +0530

    Implemented Flip Coins and finding percentage

--- UC3/UC3/Program.cs
+++ UC3/UC3/Program.cs
@@ -2,35 +2,35 @@
 
 namespace UC3
 {
-    class Program
+   public class FlipCoin
     {
+        static int HeadCount = 0;
+        static int TailCount = 0;
+        public static void FlipCoinMethod(int userinput)
+        {
+            int temp = userinput;
+            Random r = new Random();
+            while (userinput !=0)
+            {
+                double random = r.NextDouble();
+                if (random <= 0.5)
+                {
+                   TailCount = TailCount + 1;
+                }
+                else
+                {
+                    HeadCount++;
+                }
+                userinput = userinput - 1;
+            }
+            Console.WriteLine((HeadCount * 100) / temp);
+            Console.WriteLine((TailCount * 100) / temp);
+        }
         static void Main(string[] args)
         {
-            double x1, x2, y1, y2, a1, a2, b1, b2;
-            x1 = 12d;
-            x2 = 13d;
-            y1 = 11d;
-            y2 = 10d;
-            a1 = 1d;
-            a2 = 3d;
-            b1 = 1d;
-            b2 = 10d;
-            var length1 = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-            var length2 = Math.Sqrt((Math.Pow(a1 - a2, 2) + Math.Pow(b1 - b2, 2)));
-            Console.WriteLine(length1);
-            Console.WriteLine(length2);
-            if (length1 <= length2)
-            {
-                Console.WriteLine("Line2 is longer than Line1");
-            }
-            else if(length1 >= length2)
-            {
-                Console.WriteLine("Line1 is longer than Line2");
-            }
-            else
-            {
-                Console.WriteLine("Line1 and Line2 are of same length");
-            }
+            Console.WriteLine("Enter the coin flip= ");
+            int flip=Convert.ToInt32(Console.ReadLine());
+            FlipCoin.FlipCoinMethod(flip);
         }
     }
 }
